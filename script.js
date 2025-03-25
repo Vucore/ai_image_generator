b1a7345615daa2ddd49e3a02710e2eea0c408fd5
VuHuu <162543040+Vucore@users.noreply.github.com>
2025-03-25 16:30:14 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const themeToggle = document.querySelector(".theme-toggle");
 const promptBtn = document.querySelector(".prompt-btn");
+const generateBtn = document.querySelector(".generate-btn");
 const promptInput = document.querySelector(".prompt-input");
 const promptForm = document.querySelector(".prompt-form");
 const modelSelect = document.getElementById("model-select");
 const countSelect = document.getElementById("count-select");
 const ratioSelect = document.getElementById("ratio-select");
 const gridGallery = document.querySelector(".gallery-grid");
+
+const API_KEY = "";
 
 
 const examplePrompts = [
@@ -47,6 +50,68 @@ promptBtn.addEventListener("click", () => {
     promptInput.focus();
 });
 
+const getImageDimensions = (aspectRatio, baseSize = 512) => {
+    const [width, height] = aspectRatio.split("/").map(Number);
+    const scaleFactor = baseSize / Math.sqrt(width * height);
+
+    let calculatedWidth = Math.round(width * scaleFactor);
+    let calculatedHeight = Math.round(height * scaleFactor);
+
+    calculatedWidth = Math.floor(calculatedWidth / 16) * 16;
+    calculatedHeight = Math.floor(calculatedHeight / 16) * 16;
+
+    return { width : calculatedWidth, height : calculatedHeight };
+};
+
+const updateImageCard = (imgIndex, imgUrl) => {
+    const imgCard = document.getElementById(`img-card-${imgIndex}`);
+    if (!imgCard) return;
+
+    imgCard.classList.remove("loading");
+    imgCard.innerHTML = `<img src="${imgUrl}" class="result-img">
+                        <div class="img-overlay">
+                            <a href="${imgUrl}" class="img-download-btn" download="${Date.now()}.png">
+                                <i class="fa-solid fa-download"></i>
+                            </a>
+                        </div>`
+}
+
+const generateImages = async (selectedModel, imageCount, aspectRatio, promptText) => {
+    const MODEL_URL = `https://api-inference.huggingface.co/models/${selectedModel}`;
+    const { width, height} = getImageDimensions(aspectRatio);
+    generateBtn.setAttribute("disabled", "true");
+
+    const imagePromises = Array.from({length: imageCount}, async (_, i) => {
+        try{
+            const response = await fetch(MODEL_URL, {
+                headers: {
+                    Authorization: `Bearer ${API_KEY}`,
+                    "Content-Type": "application/json",
+                    "x-use-cache": "false",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    inputs: promptText,
+                    parameters: {width, height},
+                    option: {wait_for_model : true, user_cache: false},
+                }),
+            });
+            if(!response.ok) {
+                throw new Error(await response.json())?.error;
+            }
+
+            const result = await response.blob();
+            updateImageCard(i , URL.createObjectURL(result));
+        }catch (error) {
+            console.log(error)
+            const imgCard = document.getElementById(`img-card-${i}`);
+            imgCard.querySelector(".status-text").textContent = "Generation failed: Check console for more details.";
+        }
+    })
+
+    await Promise.allSettled(imagePromises);
+    generateBtn.removeAttribute("disabled");
+};
 
 const createImageCards = (selectedModel, imageCount, aspectRatio, promptText) => {
     gridGallery.innerHTML = "";
@@ -57,9 +122,10 @@ const createImageCards = (selectedModel, imageCount, aspectRatio, promptText) =>
                             <i class="fa-solid fa-triangle-exclamation"></i>
                             <p class="status-text">Generating...</p>
                         </div>
-                        <img src="test.png" class="result-img">   
                     </div>`
     }
+
+    generateImages(selectedModel, imageCount, aspectRatio, promptText);
 }
 
 const handleFormSubmit = (e) => {
